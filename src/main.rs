1d95a0aa4e9037fcce3fa162e3fe07d228dafde3
Bananababooy <48812049+bananababoo@users.noreply.github.com>
2024-07-14 15:55:38 -0500

    map

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::cmp::{max, min};
-use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
+use rltk::{GameState, RandomNumberGenerator, RGB, Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use specs_derive::Component;
 
@@ -16,18 +15,18 @@ struct Renderable {
     bg: RGB,
 }
 
-#[derive(Component)]
-struct LeftMover {}
-
 struct State {
     ecs: World
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-
+        
         player_input(self, ctx);
         self.run_systems();
+
+        let map =  self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -41,21 +40,60 @@ impl GameState for State {
 
 impl State {
     fn run_systems(&mut self){
-        let mut lw = LeftWalker{};
-        lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
 
-struct LeftWalker {}
+#[derive(PartialEq,Copy,Clone)]
+enum TileType {
+    Wall, Floor
+}
 
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>,
-                       WriteStorage<'a, Position>);
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty,pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79}
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    for x in 0..80 {
+        map[xy_idx(x,0)] = TileType::Wall;
+        map[xy_idx(x,49)] = TileType::Wall
+    }
+    for y in 0..50 {
+        map[xy_idx(0,y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = RandomNumberGenerator::new();
+    for _i in 0..100 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk){
+    let mut x = 0;
+    let mut y = 0;
+    for tile in map.iter(){
+        match tile{
+            TileType::Floor => {
+                ctx.set(x,y,RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'))
+            },
+            TileType::Wall => {
+                ctx.set(x,y,RGB::from_f32(0.0, 1.0, 1.0), RGB::from_f32(0.,0.,0.), rltk::to_cp437('#'))
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
         }
     }
 }
@@ -66,10 +104,16 @@ struct Player {}
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World){
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-
+    
     for(pos, _player) in (&mut positions, &mut players).join(){
-        pos.x = min(79, max(0, delta_x + pos.x));
-        pos.y = min(49, max(0, delta_y + pos.y));
+        let x = (delta_x + pos.x).clamp(0, 79);
+        let y = (delta_y + pos.y).clamp(0, 49);
+        let map = ecs.fetch::<Vec<TileType>>();
+        
+        if map[xy_idx(x,y,)] != TileType::Wall { // if not colliding with wall
+            pos.x = (delta_x + pos.x).clamp(0, 79);
+            pos.y = (delta_y + pos.y).clamp(0, 49);
+        }
     }
 
 }
@@ -90,7 +134,7 @@ fn player_input(gs: &mut State, ctx: &mut Rltk){
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let contex = RltkBuilder::simple80x50()
+    let context = RltkBuilder::simple80x50()
         .with_title("BananaLike")
         .build()?;
     let mut gs = State{
@@ -98,8 +142,9 @@ fn main() -> rltk::BError {
     };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
@@ -112,19 +157,7 @@ fn main() -> rltk::BError {
         .with(Player {})
         .build();
 
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('☺'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(LeftMover{})
-            .build();
-    }
 
-    rltk::main_loop(contex, gs)
+    rltk::main_loop(context, gs)
 
 }
